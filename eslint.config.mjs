b2cbcafd272ff0e0905ef426c690beee3b65c104
super nova <teozic@proton.me>
2025-05-13 11:46:18 +0300

    property is changed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,4 +4,19 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
-export default defineConfig([]);
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    rules: {},
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+]);
